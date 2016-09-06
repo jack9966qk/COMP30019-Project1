f2c5264b3079c2e7cd4e695d56ca6997226c0a9b
Jack <Jack@Jacks-MacBook-Pro.local>
2016-09-06 18:51:40 +1000

    Added light, water and some shaders

---
+++ Grpahics Project 1/Assets/ShaderScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShaderScript : MonoBehaviour
+{
+    public Shader shader;
+	public PointLight[] pointLights;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        // Add a MeshRenderer component. This component actually renders the mesh that
+        // is defined by the MeshFilter component.
+        MeshRenderer renderer = this.gameObject.AddComponent<MeshRenderer>();
+        renderer.material.shader = shader;
+    }
+
+    // Called each frame
+    void Update()
+    {
+        // Get renderer component (in order to pass params to shader)
+        MeshRenderer renderer = this.gameObject.GetComponent<MeshRenderer>();
+
+        // Pass updated light positions to shader
+		Color[] pointLightColors = new Color[pointLights.Length];
+		Vector4[] pointLightPositions = new Vector4[pointLights.Length];
+		for (int i = 0; i < pointLights.Length; i++) {
+			PointLight light = pointLights [i];
+			pointLightColors [i] = light.color;
+			pointLightPositions [i] = light.GetWorldPosition ();
+		}
+
+		renderer.material.SetInt ("_NumPointLights", pointLights.Length);
+ 		renderer.material.SetColorArray ("_PointLightColors", pointLightColors);
+    }
+
+}
--- Grpahics Project 1/Assets/SunMovement.cs
+++ Grpahics Project 1/Assets/SunMovement.cs
@@ -3,17 +3,17 @@ using System.Collections;
 
 public class SunMovement : MonoBehaviour {
 
-	public Vector3 intialPos;
+	public Vector3 initialPos;
 	public Vector3 centrePos;
 	public int speed;
 
 	// Use this for initialization
 	void Start () {
-	
+		this.transform.position = initialPos;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		transform.RotateAround (centrePos, Vector3.forward, Time.deltaTime * speed);
 	}
 }
